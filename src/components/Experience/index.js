e6a1f2831493be1efecb459400d888918271aa4a
AndrewTaoWen <andrewwen6@gmail.com>
2023-11-17 12:57:21 -0500

    fixing text on portfolio page

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -37,17 +37,14 @@ const Experience = () => {
                     experience.map((port, idx) => {
                         return (
                             <span className="image-box" key={idx}>
-                                <img 
-                                src={port.cover}
-                                className="experience-image"
-                                alt="experience" />
+                                <img
+                                    src={port.cover}
+                                    className="experience-image"
+                                    alt="experience" />
                                 <div className="content">
                                     <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
+                                    <button className="btn" onClick={() => window.open(port.url)}>View</button>
                                 </div>
                             </span>
                         )
